8d788de9b3a022981f7fc5af1fbb6473589a5270
Bryan Kroesbeek <BryanK.B96@gmail.com>
2016-12-02 12:28:25 +0100

    Optimised algorithm for a major performance boost

--- SortingAlgorithms/SortingAlgorithms/MergeSort.cs
+++ SortingAlgorithms/SortingAlgorithms/MergeSort.cs
@@ -2,49 +2,56 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SortingAlgorithms {
     public static class MergeSort {
         private static T[] MergeLists<T>(T[] firstHalf, T[] secondHalf, Func<T, T, bool> sortCondition) {
-            T[] list = new T[firstHalf.Length + secondHalf.Length];
+            int firstHalfLength = firstHalf.Length;
+            int secondHalfLength = secondHalf.Length;
+
+            T[] list = new T[firstHalfLength + secondHalfLength];
 
             int firstHalfIndex = 0;
             int secondHalfIndex = 0;
-
-            for (int i = 0; i < firstHalf.Length + secondHalf.Length; i++) {
-                if (firstHalfIndex < firstHalf.Length && secondHalfIndex < secondHalf.Length) {
-                    if (sortCondition(firstHalf[firstHalfIndex], secondHalf[secondHalfIndex])) {
-                        list[i] = firstHalf[firstHalfIndex];
-                        firstHalfIndex++;
-                    } else {
-                        list[i] = secondHalf[secondHalfIndex];
-                        secondHalfIndex++;
-                    }
-                } else {
-                    if (firstHalfIndex >= firstHalf.Length) {
-                        list[i] = secondHalf[secondHalfIndex];
-                        secondHalfIndex++;
-                    } else if (secondHalfIndex >= secondHalf.Length) {
-                        list[i] = firstHalf[firstHalfIndex];
-                        firstHalfIndex++;
-                    }
+            int index = 0;
+            
+            for (; index < firstHalfLength + secondHalfLength && firstHalfIndex < firstHalfLength && secondHalfIndex < secondHalfLength; index++) {
+                if (sortCondition(firstHalf[firstHalfIndex], secondHalf[secondHalfIndex])) {
+                    list[index] = firstHalf[firstHalfIndex];
+                    firstHalfIndex++;
+                    continue;
                 }
+                list[index] = secondHalf[secondHalfIndex];
+                secondHalfIndex++;
+            }
+
+            if (firstHalfIndex < firstHalfLength)
+                return AddFromList(list, firstHalf, firstHalfLength, firstHalfIndex, index);
+            return AddFromList(list, secondHalf, secondHalfLength, secondHalfIndex, index);
+        }
+
+        private static T[] AddFromList<T>(T[] list, T[] half, int halfLength, int halfIndex, int generalIndex) {
+            for (int i = halfIndex; i < halfLength; i++) {
+                list[generalIndex] = half[i];
+                generalIndex++;
             }
             return list;
         }
 
+        private static T[] Sorter<T>(T[] elementList, int lower, int upper, Func<T, T, bool> sortCondition) {
+            if (upper - lower <= 1)
+                return GetElementsFrom(elementList, lower, upper);
+
+            int length = upper - lower;
+            int middle = lower + length / 2;
+
+            return MergeLists(Sorter(elementList, lower, middle, sortCondition), Sorter(elementList, middle, upper, sortCondition), sortCondition);
+        }
+
         public static T[] Sort<T>(T[] elementList, Func<T, T, bool> sortCondition) {
-            if (elementList.Length <= 1)
-                return elementList;
-
-            int length = elementList.Length;
-            int middle = length / 2;
-
-            T[] firstarray = elementList.GetElementsFrom(0, middle);
-            T[] secondArray = elementList.GetElementsFrom(middle, length);
-
-            return MergeLists(Sort(firstarray, sortCondition), Sort(secondArray, sortCondition), sortCondition);
+            return Sorter(elementList, 0, elementList.Length, sortCondition);
         }
 
         private static T[] GetElementsFrom<T>(this T[] elementList, int lower, int upper) {
